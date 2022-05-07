c8a0ca6ca69d55eb846d1cfbd15df99dbc1b9ab5
mart-godoy <mart.godoy@duocuc.cl>
2022-05-07 00:51:21 -0400

    Implementando páginas con formularios y validaciones

--- Js/validacionrut.js
+++ Js/validacionrut.js
@@ -12,7 +12,7 @@ function checkRut(rut) {
     rut.value = cuerpo + '-'+ dv
     
     // Si no cumple con el mínimo ej. (n.nnn.nnn)
-    if(cuerpo.length < 7) { rut.setCustomValidity("RUT Incompleto"); return false;}
+    if(cuerpo.length < 7) { rut.setCustomValidity("Rut Incompleto"); return false;}
     
     // Calcular Dígito Verificador
     suma = 0;
@@ -40,7 +40,7 @@ function checkRut(rut) {
     dv = (dv == 0)?11:dv;
     
     // Validar que el Cuerpo coincide con su Dígito Verificador
-    if(dvEsperado != dv) { rut.setCustomValidity("RUT Inválido"); return false; }
+    if(dvEsperado != dv) { rut.setCustomValidity("Rut Inválido"); return false; }
     
     // Si todo sale bien, eliminar errores (decretar que es válido)
     rut.setCustomValidity('');
